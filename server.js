9212a6a2e4473d925229a99e3f6be33721ffedf0
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2016-10-01 12:35:46 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express=require('express');
+
+var app=express();
+
+app.get('/', function(req, res){
+    res.send('Hello, World!!!');
+});
+
+var port=process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log('The server is running on port '+port+'...');
+});

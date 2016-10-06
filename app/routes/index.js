43bd2838a31796f62214fb1520f99e30785f1ba2
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2016-10-07 00:59:44 +0530

    Almost donegit add .  Might have broken something

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,6 @@ var muri='mongodb://localhost:27017/localUsers';
 
 module.exports=function(app, passport){
 
-    app.get('/', isauth, function(req, res){
-        res.render('home', {name: req.user.name});
-    });
-     
     app.get('/account', isauth, function(req, res){
         res.send(JSON.stringify(req.user, null, 2));
     });
@@ -25,7 +21,7 @@ module.exports=function(app, passport){
         res.redirect('/');
     });
 
-    app.get('/dashboard', isauth, function(req, res){
+    app.get('/', isauth, function(req, res){
         mongo.connect(muri, function(err, db){
             if(err)
                 throw err;
@@ -37,24 +33,52 @@ module.exports=function(app, passport){
                     else{
                         var polls=[];
                         var ctr=0;
-                        arr[0].polls.forEach(function(val ){
-                            db.collection('polls').find({_id: val}, {op: false}).toArray(function(err, docs){
-                                if(err)
-                                    throw err;
-                                polls.push(docs[0]);
-                                ctr++;
-                                if(ctr==arr[0].polls.length){
-                                    res.render('dash', {name: arr[0].name, polls: polls});
-                                }
+                        if(arr[0].polls.length){
+                            arr[0].polls.forEach(function(val ){
+                                db.collection('polls').find({_id: val}, {op: false}).toArray(function(err, docs){
+                                    if(err)
+                                        throw err;
+                                    polls.push(docs[0]);
+                                    ctr++;
+                                    if(ctr==arr[0].polls.length){
+                                        res.render('dash', {name: arr[0].name, polls: polls});
+                                    }
+                                });
                             });
-                        });
+                        }
+                        else{
+                            res.render('nopoll', {name: req.user.name});
+                        }
                     }
                 });
             }
         });
     });
 
-    app.get('/poll/:id', function(req, res){
+    app.get('/removePoll/:id', isauth, function(req, res){
+        var id = req.params.id;
+        mongo.connect(muri, function(err, db){
+            if(err)
+                throw err;
+            else{
+                db.collection('users').find({polls: mongo.ObjectId(id)}, {_id: false, id: true}).toArray(function(err, arr){
+                    if(err)
+                        throw err;
+                    else{
+                        if(arr.length==0)
+                            res.send('<html><body><h4>poll deleted...  <a href="/">Go</a> to home page.</h4><body></html>');
+                        else{
+                            db.collection('users').update({id: arr[0].id}, {$pull: {polls: mongo.ObjectId(id)}});
+                            db.collection('polls').remove({_id: mongo.ObjectId(id)});
+                            res.send('<html><body><h4>poll deleted...  <a href="/">Go</a> to home page.</h4><body></html>');
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    app.get('/poll/:id', isauth, function(req, res){
         mongo.connect(muri, function(err, db){
             if(err)
                 throw err;
@@ -64,9 +88,37 @@ module.exports=function(app, passport){
                         throw er;
                     else{
                         if(ar.length>0)
-                            res.render('poll',{polls: ar});
+                            res.render('polla',{polls: ar, id: req.params.id});
                     }
                 });
+            }
+        });
+    });
+
+    app.get('/poll/:id', isauth, function(req, res){
+        mongo.connect(muri, function(err, db){
+            if(err)
+                throw err;
+            else{
+                db.collection('polls').find({_id: mongo.ObjectId(req.params.id)}, {_id: false}).toArray(function(er, ar){
+                    if(er)
+                        throw er;
+                    else{
+                        if(ar.length>0)
+                            res.render('polla',{polls: ar, id: req.params.id});
+                    }
+                });
+            }
+        });
+    });
+
+    app.get('/vote/:id/:ind', function(req, res){
+        mongo.connect(muri, function(err, db){
+            if(err)
+                throw err;
+            else{
+                db.collection('polls').update({_id: mongo.ObjectId(req.params.id, "op.st": req.params.ind)}, {$set: {"op.$.vot" : {$inc: 1}}});
+                res.redirect('/');
             }
         });
     });
@@ -98,7 +150,7 @@ module.exports=function(app, passport){
                     else{
                         var id=results.ops[0]._id;
                         db.collection('users').update({id: req.user.id}, {$addToSet:{polls: id}});
-                        res.redirect('/dashboard');
+                        res.redirect('/');
                     }
                 });
             }
--- t.js
+++
@@ -1,1 +0,0 @@
-console.log((require('mongodb')).ObjectId('57f4d8ec67c7cf155268e16c'));

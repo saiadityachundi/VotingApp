c01e74a348aec534bdc322c66e396317aa1132e1
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2016-10-30 20:56:41 +0530

    Aalmost done...

--- app/routes/index.js
+++ app/routes/index.js
@@ -58,6 +58,38 @@ module.exports=function(app, passport){
         });
     });
 
+    app.post('/', isauth, function(req, res){
+        //var op=req.body.opt.split(';');
+        
+        var ob={
+            qu: req.body.qu,
+            op:[]
+        };
+
+        ob.op.push({st: req.body.op1, vot: 0});
+        ob.op.push({st: req.body.op2, vot: 0});
+
+        /*op.forEach(function(val){
+            ob.op.push({st: val, vot: 0});
+        });*/
+
+        mongo.connect(muri, function(err, db){
+            if(err)
+                throw err;
+            else{
+                db.collection('polls').insert(ob, function(err, results){
+                    if(err)
+                        throw err;
+                    else{
+                        var id=results.ops[0]._id;
+                        db.collection('users').update({id: req.user.id}, {$addToSet:{polls: id}});
+                        res.redirect('/');
+                    }
+                });
+            }
+        });
+    });
+
     app.get('/removePoll/:id', isauth, function(req, res){
         var id = req.params.id;
         mongo.connect(muri, function(err, db){
@@ -142,6 +174,17 @@ module.exports=function(app, passport){
                     }
                 });
             }
+        });
+    });
+
+    app.post('/poll/:id', function(req, res){
+        var st=req.body.opt;
+        mongo.connect(muri, function(err, db){
+            if(err)
+                throw err;
+            db.collection('polls').update({_id: mongo.ObjectId(req.params.id), "op.st": st}, {$inc: {"op.$.vot": 1}}, function(err){
+                res.redirect('/poll/'+req.params.id);
+            });
         });
     });
 
